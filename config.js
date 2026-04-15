0d553f409056b75c4c112daeb4df5e6c390bde88
Leandro <leandro@eva-ci.com>
2026-04-15 12:42:44 -0300

    Connect cadetes app to Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CADETES_APP_CONFIG = {
   pharmacyName: "Farmacia",
-  appsScriptUrl: ""
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzW0h55Vl48owbpZzsv3gjZFc-yIN1ohUGdogF-cssS57nXyl6Z3_-wqM0PLrm1N5y_Hw/exec"
 };
